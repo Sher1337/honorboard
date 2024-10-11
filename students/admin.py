7fb14635d8fcba2b6b36442a6700f6360e501945
Egor Sher <utizenrp@gmail.com>
2024-10-12 02:47:45 +0700

    2.4dja

--- students/admin.py
+++ students/admin.py
@@ -3,9 +3,14 @@ from .models import Student, Nomination
 
 @admin.register(Student)
 class StudentAdmin(admin.ModelAdmin):
-    list_display = ('first_name', 'last_name')
+    list_display = ('first_name', 'last_name', 'get_nominations')
     search_fields = ('first_name', 'last_name')
     list_filter = ('nominations',)
+
+    def get_nominations(self, obj):
+        return ", ".join([nomination.name for nomination in obj.nominations.all()])
+    get_nominations.short_description = 'Nominations'
+
 
 @admin.register(Nomination)
 class NominationAdmin(admin.ModelAdmin):
